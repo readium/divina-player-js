3e879aea8993787d20a7b40d13f14e34b518c068
Pierre Leroux <pierre@leroux.tech>
2020-07-23 16:33:08 +0200

    reset last commit

--- src/StoryBuilder/PageNavigator.js
+++ src/StoryBuilder/PageNavigator.js
@@ -193,7 +193,7 @@ export default class PageNavigator extends LayerPile {
 		}
 
 		// If required, do something with the page change information (e.g. signal it via an event)
-		const customData = { pageIndex: this.pageIndex, nbOfPages: this.nbOfPages, currentPage: this.currentPage }
+		const customData = { pageIndex: this.pageIndex, nbOfPages: this.nbOfPages }
 		this._eventEmitter.emit("pagechange", customData)
 	}
 
